0f21d12e98b84eefdc27d1239a0ec41374b29080
Aditya Dahake <dahakeadi15@gmail.com>
2025-02-05 23:23:57 +0530

    fixes based on solution

--- main.py
+++ main.py
@@ -5,16 +5,14 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill(pygame.Color("black"))
-        pygame.display.flip()
 
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+        screen.fill("black")
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
